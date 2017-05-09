68dba33cb887737a68c7b25edaf5ca50aeeb9739
makaer15 <makaer15@gmail.com>
2017-05-09 03:38:31 +0300

    d

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.post('/webhook/', function(req, res) {
 					question_favmanager = text
 					football = false
 					question_favmanager = false
-					sendText(sender, "İlgi alanı: Futbol" + " Tutulan takım: " + question_teamVal +" Favori Oyuncular: " + question_favplayers + " En iyi teknik direktör: " + question_favmanager)
+					sendText(sender, "İlgi alanı: Futbol" + " Tutulan takım: " + question_teamVal +" Favori Oyuncular: " + question_favplayersVal + " En iyi teknik direktör: " + question_favmanagerVal)
 					football = false
 					question_team = false
 					question_favplayers = false
