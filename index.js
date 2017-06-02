5b2be9f94e3924af9cb626e084c6b2fd54b1e4f5
adamthahir <adam.thahir@gmail.com>
2017-06-02 08:22:58 +0300

    Brought back the convosational Parts Added match with no names

--- index.js
+++ index.js
@@ -482,203 +482,222 @@ function isInterestExists(interestname){
 
 
 
-// app.post('/webhook/', function(req, res) {
-// 	let messaging_events = req.body.entry[0].messaging
-// 	for(let i = 0; i < messaging_events.length; i++) {
-// 		let event = messaging_events[i];
-// 		let sender = event.sender.id
-// 		if(event.message && event.message.text) {
-// 			let text = event.message.text
-// 			if (text.includes("dis")){
-// 				sendText(sender,getUsername(sender))
-// 			} else if(text.includes("futbol")) {
-// 				football = true
-// 				sendText(sender, "Hangi takımı tutuyorsunuz?")
-// 				question_team = true
-// 			} else if(text.includes("kitap")) {
-// 				books = true
-// 				sendText(sender, "Hangi tür kitaplardan hoşlanırsın?")
-// 				question_booktype = true
-// 			} else if(text.includes("ilgi")) {
-// 				sendText(sender, "İlgi alanınız veya yapmaktan hoşlandığınız şey nedir?");
-// 				ilgi = true
-// 			} else if(text.includes("müzik")) {
-// 				music = true
-// 				sendText(sender, "Hangi tür müziklerden hoşlanırsın")
-// 				question_musicType = true	
-// 			} else if(text.includes("araba")) {
-// 				car = true
-// 				sendText(sender, "Hangi tür arabalardan hoşlanırsın")
-// 				question_carType = true	
-// 			} else if (text.includes("film")) {
-// 				movie=true
-// 				sendText(sender, "Hangi tür filmlerden hoşlanırsın")
-// 				question_movieType = true	
-//             } else if (text.includes("oyun")) {
-// 				game=true
-// 				sendText(sender, "Hangi tür oyunlardan hoşlanırsın")
-// 				question_gameType = true	
-//             }
-// 			else if(books) { // ******************************************************************************************
-// 				if(question_booktype) {
-// 					question_booktypeVal = text
-// 					question_booktype = false
-// 					question_localorforeign = true
-// 					sendText(sender, question_booktypeVal + " kitaplarından yerli mi yabancı mı daha çok seversin?")
-// 				} else if(question_localorforeign) {
-// 					question_localorforeignVal = text
-// 					question_localorforeign = false
-// 					question_favAuthor = true
-// 					sendText(sender, "Sevdiğin yazarlardan 2 tane söyler misin?")
-// 				} else if(question_favAuthor) {
-// 					question_favAuthor = false
-// 					question_favAuthorVal = text
-// 					question_lastBookRead = true
-// 					sendText(sender, "En son okuduğun kitabı söyler misin?")
-// 				} else if(question_lastBookRead) {
-// 					question_lastBookRead = false
-// 					question_lastBookReadVal = text
-// 					books = false
-// 					sendText(sender, "İlgi alanı: Kitaplar.\nTür: " + question_booktypeVal + "\nYerli/Yabancı: " + question_localorforeignVal +  "\nYazarlar: " + question_favAuthorVal + "\nEn son okunan kitap: " + question_lastBookReadVal)
-// 				}
-// 			} else if(football) { // ******************************************************************************************
-// 				if(question_team) {
-// 					question_teamVal = text
-// 					sendText(sender, "En sevdiğiniz 2 oyuncuyu söyler misiniz?")
-// 					question_team = false
-// 					question_favplayers = true
-// 				} else if(question_favplayers) {
-// 					question_favplayersVal = text
-// 					sendText(sender, "Sizce gelmiş geçmiş en iyi teknik direktör kim?")
-// 					question_favplayers = false
-// 					question_favmanager = true
-// 				} else if(question_favmanager) {
-// 					question_favmanagerVal = text
-// 					football = false
-// 					question_favmanager = false
-// 					sendText(sender, "İlgi alanı: Futbol\n" + "Tutulan takım: " + question_teamVal +"\nFavori Oyuncular: " + question_favplayersVal + "\nEn iyi teknik direktör: " + question_favmanagerVal)
+app.post('/webhook/', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for(let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i];
+		let sender = event.sender.id
+		if(event.message && event.message.text) {
+			let text = event.message.text
+			/*if (text.includes("dis")){
+				sendText(sender,getUsername(sender))
+			} else*/
+      if (text.includes("merhaba") || text.includes("Merhaba")){
+        getUsername(sender,true);
+      }else if (text.includes("arkadaş")){
+         getPeopleWithInterests(sender);
+				sendText(sender, "Arıyoruz.")
+      }else if(text.includes("futbol")) {
+        getUsername(sender,false);
+        addInterest2(text);
+				football = true
+				sendText(sender, "Hangi takımı tutuyorsunuz?")
+				question_team = true
+			} else if(text.includes("kitap")) {
+        getUsername(sender,false);
+        addInterest2(text);
+				books = true
+				sendText(sender, "Hangi tür kitaplardan hoşlanırsın?")
+				question_booktype = true
+			} else if(text.includes("ilgi")) {
+				sendText(sender, "İlgi alanınız veya yapmaktan hoşlandığınız şey nedir?");
+				ilgi = true
+			} else if(text.includes("müzik")) {
+        getUsername(sender,false);
+        addInterest2(text);
+				music = true
+				sendText(sender, "Hangi tür müziklerden hoşlanırsın")
+				question_musicType = true	
+			} else if(text.includes("araba")) {
+        getUsername(sender,false);
+        addInterest2(text);
+				car = true
+				sendText(sender, "Hangi tür arabalardan hoşlanırsın")
+				question_carType = true	
+			} else if (text.includes("film")) {
+        getUsername(sender,false);
+        addInterest2(text);
+				movie=true
+				sendText(sender, "Hangi tür filmlerden hoşlanırsın")
+				question_movieType = true	
+            } else if (text.includes("oyun")) {
+              getUsername(sender,false);
+        addInterest2(text);
+				game=true
+				sendText(sender, "Hangi tür oyunlardan hoşlanırsın")
+				question_gameType = true	   
+            }
+			else if(books) { // ******************************************************************************************
+        
+				if(question_booktype) {
+					question_booktypeVal = text
+					question_booktype = false
+					question_localorforeign = true
+					sendText(sender, question_booktypeVal + " kitaplarından yerli mi yabancı mı daha çok seversin?")
+				} else if(question_localorforeign) {
+					question_localorforeignVal = text
+					question_localorforeign = false
+					question_favAuthor = true
+					sendText(sender, "Sevdiğin yazarlardan 2 tane söyler misin?")
+				} else if(question_favAuthor) {
+					question_favAuthor = false
+					question_favAuthorVal = text
+					question_lastBookRead = true
+					sendText(sender, "En son okuduğun kitabı söyler misin?")
+				} else if(question_lastBookRead) {
+					question_lastBookRead = false
+					question_lastBookReadVal = text
+					books = false
+					sendText(sender, "İlgi alanı: Kitaplar.\nTür: " + question_booktypeVal + "\nYerli/Yabancı: " + question_localorforeignVal +  "\nYazarlar: " + question_favAuthorVal + "\nEn son okunan kitap: " + question_lastBookReadVal)
+				}
+			} else if(football) { // ******************************************************************************************
+        
+				if(question_team) {
+					question_teamVal = text
+					sendText(sender, "En sevdiğiniz 2 oyuncuyu söyler misiniz?")
+					question_team = false
+					question_favplayers = true
+				} else if(question_favplayers) {
+					question_favplayersVal = text
+					sendText(sender, "Sizce gelmiş geçmiş en iyi teknik direktör kim?")
+					question_favplayers = false
+					question_favmanager = true
+				} else if(question_favmanager) {
+					question_favmanagerVal = text
+					football = false
+					question_favmanager = false
+					sendText(sender, "İlgi alanı: Futbol\n" + "Tutulan takım: " + question_teamVal +"\nFavori Oyuncular: " + question_favplayersVal + "\nEn iyi teknik direktör: " + question_favmanagerVal)
 					
-// 					football = false
-// 					question_team = false
-// 					question_favplayers = false
-// 					question_favmanager = false
+					football = false
+					question_team = false
+					question_favplayers = false
+					question_favmanager = false
 
-// 					question_teamVal = ""
-// 					question_favplayersVal = ""
-// 					question_favmanagerVal = ""
-// 				}
-// 			} else if(ilgi) {
-// 				ilgiVal = text
-// 				ilgi = false
-// 				sendText(sender, "İlgi alanı: " + ilgiVal)
-// 			} 
+					question_teamVal = ""
+					question_favplayersVal = ""
+					question_favmanagerVal = ""
+				}
+			} else if(ilgi) {
+				ilgiVal = text
+				ilgi = false
+				sendText(sender, "İlgi alanı: " + ilgiVal)
+			} 
 
 
-// 			else if (music) {
-// 				if(question_musicType) {
-// 					question_musicTypeValue = text
-// 					question_musicType = false
-// 					question_localorforeign = true
-// 					sendText(sender, question_musicTypeValue + " müziklerden yerli mi yabancı mı daha çok seversin?")
-// 			}
+			else if (music) {
+				if(question_musicType) {
+					question_musicTypeValue = text
+					question_musicType = false
+					question_localorforeign = true
+					sendText(sender, question_musicTypeValue + " müziklerden yerli mi yabancı mı daha çok seversin?")
+			}
 
-// 			 else if(question_localorforeign) {
-// 					question_localorforeignVal = text
-// 					question_localorforeign = false
-// 					question_favSinger = true
-// 					sendText(sender, "Sevdiğin 2 tane şarkıcı söyler misin?")
-// 			} else if(question_favSinger) {
-// 					question_favSinger = false
-// 					question_faveSingerValue = text
-// 					question_favSing = true
-// 					sendText(sender, "En sevdiğiniz şarkının adı nedir?")
-// 			}
-// 			  else if(question_favSing) {
-// 					question_favSing = false
-// 					question_faceSingValue = text
-// 					music = false
-// 					sendText(sender, "İlgi alanı: Müzik.\nTür: " + question_musicTypeValue + "\nYerli/Yabancı: " + question_localorforeignVal +  "\nŞarkıcı: " + question_faveSingerValue + "\nEn sevdiğiniz şarkı: " + question_faceSingValue)
-// 					question_faceSingValue = ""
-// 					question_faveSingerValue = ""
-// 					question_localorforeignVal = ""
+			 else if(question_localorforeign) {
+					question_localorforeignVal = text
+					question_localorforeign = false
+					question_favSinger = true
+					sendText(sender, "Sevdiğin 2 tane şarkıcı söyler misin?")
+			} else if(question_favSinger) {
+					question_favSinger = false
+					question_faveSingerValue = text
+					question_favSing = true
+					sendText(sender, "En sevdiğiniz şarkının adı nedir?")
+			}
+			  else if(question_favSing) {
+					question_favSing = false
+					question_faceSingValue = text
+					music = false
+					sendText(sender, "İlgi alanı: Müzik.\nTür: " + question_musicTypeValue + "\nYerli/Yabancı: " + question_localorforeignVal +  "\nŞarkıcı: " + question_faveSingerValue + "\nEn sevdiğiniz şarkı: " + question_faceSingValue)
+					question_faceSingValue = ""
+					question_faveSingerValue = ""
+					question_localorforeignVal = ""
 					
-// 				}
+				}
 
-// 			}	
+			}	
 
 
-// 			else if (car) {
-// 				if(question_carType) {
-// 					question_carTypeValue = text
-// 					question_carType = false
-// 					question_carBrand = true
-// 					sendText(sender, "Hangi marka arabalardan hoşlanırsın?")
-// 			    }
+			else if (car) {
+				if(question_carType) {
+					question_carTypeValue = text
+					question_carType = false
+					question_carBrand = true
+					sendText(sender, "Hangi marka arabalardan hoşlanırsın?")
+			    }
 
-// 			    else if(question_carBrand) {
-// 					question_carBrandValue = text
-// 					question_carBrand = false
-// 					question_favCarModel = true
-// 					sendText(sender, "En sevdiğin araba modeli nedir?")
-// 			    } 
-// 			    else if(question_favCarModel) {
-// 					question_favCarModel = false
-// 					question_favCarModelValue = text
-// 					car = false
-// 					sendText(sender, "İlgi alanı: Araba.\nTür: " + question_carTypeValue +  "\nMarka: " + question_carBrandValue + "\nEn sevdiğiniz model: " + question_favCarModelValue)
+			    else if(question_carBrand) {
+					question_carBrandValue = text
+					question_carBrand = false
+					question_favCarModel = true
+					sendText(sender, "En sevdiğin araba modeli nedir?")
+			    } 
+			    else if(question_favCarModel) {
+					question_favCarModel = false
+					question_favCarModelValue = text
+					car = false
+					sendText(sender, "İlgi alanı: Araba.\nTür: " + question_carTypeValue +  "\nMarka: " + question_carBrandValue + "\nEn sevdiğiniz model: " + question_favCarModelValue)
 
-// 			    }
+			    }
 			 
-//             }
+            }
 
             
-//             else if (movie) {
-// 				if(question_movieType) {
-// 					question_movieTypeValue = text
-// 					question_movieType = false
-// 					question_movieName = true
-// 					sendText(sender, " Favori filmin nedir?")
-// 			    }
+            else if (movie) {
+				if(question_movieType) {
+					question_movieTypeValue = text
+					question_movieType = false
+					question_movieName = true
+					sendText(sender, " Favori filmin nedir?")
+			    }
 
-// 			    else if(question_movieName) {
-// 					question_movieName = false
-// 					question_movieNameValue = text
-// 					movie = false
-// 					sendText(sender, "İlgi alanı: film.\nTür: " + question_movieTypeValue +  "\nFavori film: " + question_movieNameValue)
+			    else if(question_movieName) {
+					question_movieName = false
+					question_movieNameValue = text
+					movie = false
+					sendText(sender, "İlgi alanı: film.\nTür: " + question_movieTypeValue +  "\nFavori film: " + question_movieNameValue)
 
-// 			    }
+			    }
 			 
-//             }
+            }
 
-//             else if (game) {
-// 				if(question_gameType) {
-// 					question_gameTypeValue = text
-// 					question_gameType = false
-// 					question_ganePlatform = true
-// 					sendText(sender, "Hangi platformda oyun oynamayı seversin?")
-// 			    }
+            else if (game) {
+				if(question_gameType) {
+					question_gameTypeValue = text
+					question_gameType = false
+					question_ganePlatform = true
+					sendText(sender, "Hangi platformda oyun oynamayı seversin?")
+			    }
 
-// 			    else if(question_ganePlatform) {
-// 					question_gamePlatformValue = text
-// 					question_ganePlatform = false
-// 					question_gameName = true
-// 					sendText(sender, "En sevdiğin oyun nedir?")
-// 			    } 
-// 			    else if(question_gameName) {
-// 					question_gameName = false
-// 					question_gameNameValue = text
-// 					game = false
-// 					sendText(sender, "İlgi alanı: Oyun.\nTür: " + question_gameTypeValue +  "\nPlatform: " + question_gamePlatformValue + "\nEn sevdiğiniz oyun: " + question_gameNameValue)
+			    else if(question_ganePlatform) {
+					question_gamePlatformValue = text
+					question_ganePlatform = false
+					question_gameName = true
+					sendText(sender, "En sevdiğin oyun nedir?")
+			    } 
+			    else if(question_gameName) {
+					question_gameName = false
+					question_gameNameValue = text
+					game = false
+					sendText(sender, "İlgi alanı: Oyun.\nTür: " + question_gameTypeValue +  "\nPlatform: " + question_gamePlatformValue + "\nEn sevdiğiniz oyun: " + question_gameNameValue)
 
-// 			    }
+			    }
 			 
-//             } else {
-// 				sendText(sender, "Merhaba, İlgi alanlarından birisine ihtiyacım var. Şunlardan birisini seçebilirsin: futbol, kitap, müzik, araba, film, oyun. Kendi ilgi alanını söylemek için lütfen ilgi yaz.")	
-// 			}
-// 		}
-// 	}
-// 	res.sendStatus(200)
-// })
+            } else {
+				sendText(sender, "Merhaba, \"ilgi\" yazıp ilgi alanını söyleyebilirsin veya \"arkadaş\" yazarak sana önerdiğimiz arkadaşları görebilirsin."			}
+		}
+	}
+	res.sendStatus(200)
+})
 
 
 
